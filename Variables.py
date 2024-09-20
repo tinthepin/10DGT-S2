776970fed6dd3cba1039436fb48b5375fe1afc0d
tinthepin <trystan.henwood@kingsway.school.nz>
2024-09-20 14:38:20 +1200

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,25 @@
+# Showing how variables work
+# Author: Trystan Henwood
+# Date: 20 September 2024
+# Version: 1
+
+
+# Different types of variables 
+# Store a number 
+num_9 = 11 # assigning 11 to num_9
+print(num_9) # This displays what num_9 contains
+
+# Store a string
+name = "Titus is weaaaakkk"
+print(name)
+name = "Sike thats the wrong number" # Reassigning value to an existing variable
+print(name)
+
+# Do Calculations in a variable and store the result
+sum = 5 + 5 # Leave spaces between sums not like sum=5+5
+print(sum)
+sum =" 5 + 5 =  "
+
+# Adding strings together is called concatnation
+concatenation = name + sum 
+print(concatenation)
