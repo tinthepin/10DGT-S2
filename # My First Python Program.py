70decc1ac47a9df8ee4a09e48f6851b3b0adaddd
tinthepin <trystan.henwood@kingsway.school.nz>
2024-09-20 14:37:34 +1200

    Add files via upload

---
+++ # My First Python Program.py
@@ -0,0 +1,12 @@
+# My First Python Program
+# Author: Trystan Henwood
+# Date:2024-09-18
+# Version 1.0
+
+
+# This is a comment. It starts with a #.
+# print out a welcome message.
+print('Hello World!\n') # \n creates a line break
+print() # This creates a line break
+print('Congratulations on running your first program!')
+
