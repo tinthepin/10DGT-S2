b438195b66bc01f90a06b7c514af5070764682af
tinthepin <trystan.henwood@kingsway.school.nz>
2024-09-25 09:59:22 +1200

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,22 @@
+# Loops and indents
+# Author: Trystan Henwood
+# Date: 20 September 2024
+# Version: 1
+
+
+# for loops. Repeat for a set number of times.
+# for start the loop
+# next is the name of the loop
+# in range tells the loop how many times to run
+# the number in the () is how many times in repeats
+num_repeat = (3)
+for Titus_loop in range(num_repeat):
+    print("haha")
+   
+# While loop. runs until a condition is met 
+keep_going= "" # empty variable
+while keep_going == "":
+    print("looping")
+    print("still looping")
+
+    keep_going = input("Again? Or press any other key to quit")
